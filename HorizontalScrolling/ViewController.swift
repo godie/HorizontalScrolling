682c9d79c4e9b045c6cf3ee90e20580ffd7b8768
godie <godie.mendoza@gmail.com>
2015-08-03 15:19:24 -0600

    Initial Commit

---
+++ HorizontalScrolling/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HorizontalScrolling
+//
+//  Created by Sergio J. Carrasco A. on 03/08/15.
+//  Copyright (c) 2015 Pixcomp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
